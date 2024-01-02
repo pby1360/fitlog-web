a0e8810ccd31167385f4767d36254c4e5c4e8e68
BY <pby1360@gmail.com>
2024-01-03 00:54:46 +0900

    add Program Part Item set #4

--- src/views/workout/program/WorkoutProgramPartItemSet.vue
+++ src/views/workout/program/WorkoutProgramPartItemSet.vue
@@ -5,22 +5,22 @@
         <div class="box-header">
           <p class="title">Sets</p>
           <div class="buttons">
-            <v-btn @click="">Add</v-btn>
+            <v-btn @click="showProgramPartItemSetAdd = true">Add</v-btn>
           </div>
         </div>
         <div class="box-content">
-          <!-- <div v-if="partList.length > 0" class="item-wrapper">
-            <div class="item" :class="selectedPart.id == part.id ? 'selected' : null" v-for="part in partList" :key="part.id" @click="selectPart(part)">
-              <span class="text">{{ part.workoutPartName }}</span>
+          <div v-if="programPartItemSetList.length > 0" class="item-wrapper">
+            <div class="item" :class="selectedSet.id == set.id ? 'selected' : null" v-for="set in programPartItemSetList" :key="set.id" @click="selectSet(set)">
+              <span class="text">{{ set.order }}</span>
+              <span class="text">{{ set.description }}</span>
               <div class="buttons">
-                <v-btn variant="outlined" @click="getProgramPartDetail(part.id)"><v-icon class="text" icon="mdi-magnify"></v-icon></v-btn>
-                <v-btn variant="outlined" @click="deleteProgramPart(part.id)" color="red"><v-icon class="text" icon="mdi-delete-outline"></v-icon></v-btn>
+                <!-- <v-btn variant="outlined" @click="deleteProgramPart(part.id)" color="red"><v-icon class="text" icon="mdi-delete-outline"></v-icon></v-btn> -->
               </div>
             </div>
           </div>
           <div v-else class="item-wrapper">
             No sets.
-          </div> -->
+          </div>
         </div>
       </div>
     </template>
@@ -43,6 +43,24 @@
       </div>
     </template>
   </CommonLayout>
+  <v-dialog
+    v-model="showProgramPartItemSetAdd"
+    width="400"
+  >
+    <v-card>
+      <v-card-title>Add Program Part Item Set</v-card-title>
+      <v-card-text>
+        <v-text-field v-model="nextNumber" label="no" :readonly=true></v-text-field>
+        <v-text-field v-model="newProgramPartItemSet.description" label="description"></v-text-field>
+        <v-text-field v-model="newProgramPartItemSet.count" type="number" label="count"></v-text-field>
+        <v-text-field v-model="newProgramPartItemSet.restTime" type="number" label="rest time"></v-text-field>
+      </v-card-text>
+      <v-card-actions style="display: flex;">
+        <v-btn style="flex:1;" variant="flat" color="#CCCCFF" @click="addProgramPartItemSet">save</v-btn>
+        <v-btn @click="showProgramPartItemSetAdd = false" style="flex:1;" variant="flat" color="#e8e8e8">cancel</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script setup>
@@ -60,10 +78,67 @@ const props = defineProps ({
   itemId: Number,
 });
 
-// const title = computed(() => 'Program Parts > ' + program.value.name);
-const title = 'Item Sets';
+const title = computed(() => 'Item Sets > ' + programPartItem.value.name);
+
+const showProgramPartItemSetAdd = ref(false);
+
+const nextNumber = computed(() => programPartItemSetList.value.length + 1);
+const newProgramPartItemSet = ref({});
+
+const programPartItem = ref({});
+const programPartItemSetList = ref([]);
+
+const selectedSet = ref ({});
+
+const isOrderMode = ref(false);
+
+const selectSet = (set) => {
+  if (selectedSet.value.id == set.id) {
+    return;
+  }
+  // getProgramPartItemList(part.id);
+  selectedSet.value = set;
+  isOrderMode.value = false;
+};
+
+const addProgramPartItemSet = async () => {
+  if (!newProgramPartItemSet.value.count) {
+    return alert('Count를 선택하세요.');
+  }
+  if (!newProgramPartItemSet.value.restTime) {
+    return alert('Rest time을 선택하세요.');
+  }
+  store.commit('setLoading', true);
+  newProgramPartItemSet.value.workoutProgramPartItemId = props.itemId;
+  await axios.post(`/api/workout-programs/${props.programId}/parts/${props.partId}/items/${props.itemId}/sets`, newProgramPartItemSet.value)
+  .then(response => {
+    newProgramPartItemSet.value = {};
+    showProgramPartItemSetAdd.value = false;
+    getProgramPartItemSetList();
+  })
+  .catch((error) => alert('저장 실패!'))
+  .finally(() => store.commit('setLoading', false));
+};
+
+const getProgramPartItemSetList = async () => {
+  store.commit('setLoading', true);
+  await axios.get(`/api/workout-programs/${props.programId}/parts/${props.partId}/items/${props.itemId}/sets`)
+  .then(response => {
+    programPartItem.value = {
+      id: response.data.id,
+      name: response.data.name,
+    };
+    programPartItemSetList.value = response.data.WorkoutProgramPartItemSets;
+  })
+  .catch((error) => {
+    console.error(error)
+    alert('조회 실패');
+  })
+  .finally(() => store.commit('setLoading', false));
+}
 
 const init = () => {
+  getProgramPartItemSetList();
 };
 
 onMounted(() =>   init());
