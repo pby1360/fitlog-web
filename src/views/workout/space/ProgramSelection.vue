bf588d3ca0dad902ad15acd41ed85615e1a46c44
BY <pby1360@gmail.com>
2024-01-30 00:30:42 +0900

    add select item #5

--- src/views/workout/space/ProgramSelection.vue
+++ src/views/workout/space/ProgramSelection.vue
@@ -8,9 +8,24 @@
     <template #box>
       <div class="program" v-if="selectedProgram.id">
         <div class="part-list" v-for="part in partList" :key="part.id">
-          <div class="part">{{ part.workoutPartName }}</div>
+          <div class="part">
+            <span class="text">{{ part.workoutPartName }}</span>
+            <div class="buttons">
+              <v-btn variant="outlined" size="x-small" icon="mdi-menu-up"></v-btn>
+              <v-btn variant="outlined" size="x-small" icon="mdi-menu-down"></v-btn>
+              <v-btn variant="outlined" color="green" size="x-small" icon="mdi-plus" @click="getItemList(part.id, part.workoutPartId)"></v-btn>
+              <v-btn variant="outlined" color="red" size="x-small" icon="mdi-delete-outline"></v-btn>
+            </div>
+          </div>
           <div class="item-list" v-if="part.items.length > 0">
-            <div class="item" v-for="item in part.items" :key="item.id">{{ item.name }}</div>
+            <div class="item" v-for="item in part.items" :key="item.id">
+              <span class="text">{{ item.name }}</span>
+              <div class="buttons">
+                <v-btn variant="outlined" size="x-small" icon="mdi-menu-up"></v-btn>
+                <v-btn variant="outlined" size="x-small" icon="mdi-menu-down"></v-btn>
+                <v-btn variant="outlined" color="red" size="x-small" icon="mdi-delete-outline"></v-btn>
+              </div>
+            </div>
           </div>
           <div class="item-list" v-else>
             No items.
@@ -39,6 +54,26 @@
       <v-card-actions style="display: flex;">
         <v-btn style="flex:1;" variant="flat" color="#CCCCFF" @click="selectProgram">Ok</v-btn>
         <v-btn @click="showProgramSelection = false" style="flex:1;" variant="flat" color="#e8e8e8">cancel</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+  <v-dialog
+    v-model="showItemSelection"
+    width="400"
+  >
+    <v-card>
+      <v-card-title>Select Item</v-card-title>
+      <v-card-text>
+        <v-select
+          v-model="tempSeletedItem"
+          clearable
+          label="Item"
+          :items="itemComboList"
+        />
+      </v-card-text>
+      <v-card-actions style="display: flex;">
+        <v-btn style="flex:1;" variant="flat" color="#CCCCFF" @click="selectItem">Ok</v-btn>
+        <v-btn @click="showItemSelection = false" style="flex:1;" variant="flat" color="#e8e8e8">cancel</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -72,6 +107,8 @@ const getProgramList = async () => {
 /* part */
 
 const partList = ref([]);
+let selectedProgramPartId;
+
 const getPartList = async (workoutProgramId) => {
   await axios.get(`/api/workout-programs/${workoutProgramId}/parts?include=item`)
   .then(response => {
@@ -103,8 +140,41 @@ const selectProgram = () => {
   getPartList(workoutProgramId);
 }
 
+/* item */
 
+const getItemList = async (programPartId, workoutPartId) => {
+  selectedProgramPartId = programPartId;
+  await axios.get(`/api/workout-master/${user.id}/parts/${workoutPartId}/items`)
+  .then(response => {
+    itemList = response.data.items;
+    itemComboList.value = response.data.items.map(item => {
+      return {title: item.name, value: item.id};
+    });
+    showItemSelection.value = true;
+  }).catch(error => {
+    console.error(error)
+    alert('load failed!');
+  });
+}
 
+const showItemSelection = ref(false);
+const tempSeletedItem = ref();
+// let selectedItem;
+const itemComboList = ref([]);
+let itemList = [];
+
+const selectItem = () => {
+  const itemIndex = itemList.findIndex(item => item.id == tempSeletedItem.value);
+  const selectedItem = {
+    workoutPartItemId: itemList[itemIndex].id,
+    name: itemList[itemIndex].name,
+  }
+  const partIndex = partList.value.findIndex(part => part.id == selectedProgramPartId);
+  partList.value[partIndex].items.push(selectedItem);
+  tempSeletedItem.value = null;
+  showItemSelection.value = false;
+  // getPartList(itemId);
+}
 
 </script>
 
@@ -130,6 +200,26 @@ const selectProgram = () => {
       display: flex;
       justify-content: space-between;
 
+      .text {
+        margin: auto 0;
+        flex: 1;
+        font-weight: bold;
+      }
+
+      .buttons {
+        flex: 1;
+        margin-left: 0.2rem;
+        display: flex;
+        justify-content: end;
+          gap: 0.1rem;
+
+
+        button {
+          margin: auto 0;
+      }
+
+      }
+
       &:hover {
         border: solid 1px black;
       }
@@ -152,6 +242,23 @@ const selectProgram = () => {
         height: 3rem;
         display: flex;
         justify-content: space-between;
+
+        .text {
+          margin: auto 0;
+          flex: 1;
+        }
+
+        .buttons {
+          flex: 1;
+          margin-left: 0.2rem;
+          display: flex;
+          justify-content: end;
+          gap: 0.1rem;
+
+          button {
+            margin: auto 0;
+          }
+        }
 
         &:hover {
           border: solid 1px black;
